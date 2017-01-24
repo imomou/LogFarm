958bf57427938ee0f4d7af6f77b29c6a59b08502
imomou <ko.sam.wang@gmail.com>
2017-01-24 17:50:25 +1100

    update to template, remove miniprofiler ddb for now

--- base-infrastructure/Deploy-Infrastructure.ps1
+++ base-infrastructure/Deploy-Infrastructure.ps1
@@ -1,25 +1,25 @@
 ﻿param(
     $environment = (Read-Host 'Environment'),
     $ElmahTableName = (Read-Host 'Name of Elmah Table'),
-    $MiniProfilerTableName = (Read-Host 'Name of MiniProfiler Table'),
-    $Bucket = (Read-Host 'Name of MiniProfiler Table')
-
+    $Bucket = (Read-Host 'Name of the S3 bucket to store template'),
+	$ProjectName = (Read-Host 'Name of the Project')
 )
 
 $build_number = 1
 
+#Thanks to AWS Lego https://github.com/SleeperSmith/Aws-Lego
+
 . .\Deployment.ps1
 
 $tags = @(
-    @{"Key" = "Project"; "Value" = "Forensics"},
+    @{"Key" = "Project"; "Value" = $ProjectName},
     @{"Key" = "Environment"; "Value" = $environment}
 )
 
-$p1refix = New-Deployment -bucketname $Bucket -projectname "LogFarm" -version $build_number -deployroot .\assets
+$p1refix = New-Deployment -bucketname $Bucket -projectname $ProjectName -version $build_number -deployroot .\assets
 
 Get-StackLinkParameters -StackParameters @(
     @{"Key" = "ElmahTableName"; "Value" = $ElmahTableName},
-    @{"Key" = "MiniProfilerTableName"; "Value" = $MiniProfilerTableName},
     @{"Key" = "IsSubscribed"; "Value" = "subscribe"}
 ) -TemplateUrl "$($p1refix)logfarm-base.template" |
     Upsert-StackLink -StackName "$environment-Forensics" -Tags $tags
